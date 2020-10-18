a0425d8a366194424f710fdf492035b8a243986e
StartBimbo <julinhovasquez20@gmail.com>
2020-10-18 15:15:49 -0500

    Variables con VAR

---
+++ js/variables.js
@@ -0,0 +1,9 @@
+//En javascript existen 3 formas de crear variables
+//camelCase : nombreApellido
+//underscore : nombre_apellido
+//pascal case : NombreApellido
+var nombre = 'Julinho';
+var edad = 24;
+nombre = 'Renombrando a Ju';
+
+console.log('El Joven ' + nombre + ' tiene una edad de :' + edad);
